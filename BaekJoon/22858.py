d347b187343211351ee9d621f456bfb3c1ae3bc7
bigwaveBigwave <bluebiird909@naver.com>
2026-02-20 12:46:12 +0900

    순열

---
+++ BaekJoon/22858.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+s = list(map(int, input().split()))
+d = list(map(int, input().split()))
+
+d = [x - 1 for x in d]
+cur = s
+for _ in range(k):
+    prev = [0] * n
+    for i in range(n):
+        prev[d[i]] = cur[i]
+    cur = prev
+
+print(*cur)
