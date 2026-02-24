c6f382600302e97c0e17ffdfe978c007d059ee1e
bigwaveBigwave <bluebiird909@naver.com>
2026-02-24 23:57:49 +0900

    조합, 브루트포스

--- BaekJoon/2798.py
+++ BaekJoon/2798.py
@@ -2,7 +2,13 @@
 #구조화
 # 입력받은 ㄱ카드 번호들을 조합으로 세개 뽑아서 m을 안 넘으면, 최대값 갱신해서 출력
 import sys
-from
+from itertools import combinations
 input = sys.stdin.readline
 n, m = map(int, input().split())
 nn = list(map(int, input().split()))
+max_v = 0
+for comb in combinations(nn, 3):
+    if sum(comb) < m:
+        max_v = max(max_v, sum(comb))
+print(max_v)
+
